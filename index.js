db853190a6a7cb5b8fe854e83829f14d290888bc
n2gh0st <132196374+darkOwol12@users.noreply.github.com>
2024-04-16 20:31:15 +0200

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Obsługuje NGH Community"];
 
 
 let currentIndex = 0;
